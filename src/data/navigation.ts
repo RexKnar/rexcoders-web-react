45fabde6a6b55e3d1622eb7cc989c5f8fafba36f
Sobinjm <jmsobin@gmail.com>
2024-06-04 22:10:19 +0530

    Update navigation.ts

--- src/data/navigation.ts
+++ src/data/navigation.ts
@@ -1,24 +1,26 @@
 
 
 // pages navigations
-const navItems=[{
-  id: 1,
-  title: 'Home',
-  path:'/',
-  children: []
-},
-{
-  id: 2,
-  title: 'Courses',
-  path:'/courses',
-  children: []
-},
-{
-  id: 3,
-  title: 'Say Hello!',
-  path:'/contact',
-  children: []
-},];
+const navItems=[
+//   {
+//   id: 1,
+//   title: 'Home',
+//   path:'/',
+//   children: []
+// },
+// {
+//   id: 2,
+//   title: 'Courses',
+//   path:'/courses',
+//   children: []
+// },
+// {
+//   id: 3,
+//   title: 'Say Hello!',
+//   path:'/contact',
+//   children: []
+// },
+               ];
 
 
 
