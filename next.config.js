304722c038ae3b1ee99f92510c1ab8fc6d073533
Sobinjm <jmsobin@gmail.com>
2025-11-13 16:56:33 +0530

    temp fix

--- next.config.js
+++ next.config.js
@@ -2,10 +2,10 @@
 const nextConfig = {
   // swcMinify: true,
   reactStrictMode: false,
-  // output: 'export'
-  // images: {
-  //   unoptimized: true
-  // }
+  output: 'export',
+  images: {
+    unoptimized: true
+  }
   // exportTrailingSlash: true,
   // exportPathMap: async function (defaultPathMap) {
   //   return {
